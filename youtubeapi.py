60ba76815bafa91913550a2f903511af971838e7
unknown <asuskonotopo@gmail.com>
2022-08-28 15:44:48 +0300

    exceptions

--- youtubeapi.py
+++ youtubeapi.py
@@ -5,16 +5,24 @@ import requests
 
 
 service = build("youtube", "v3", developerKey="AIzaSyCBIvms7wo-pRvflwinAfP8NuWxarB-6rU")
+headers = {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
 
-def get_channel_info(url):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.content, "lxml")
-    meta = soup.find("meta", itemprop="channelId")
+def get_channel_info(url, headers):
+    try:
+        req = requests.get(url, headers=headers)
+        soup = BeautifulSoup(req.content, "lxml")
+        meta = soup.find("meta", itemprop="channelId")
 
-    chl_name = soup.find("meta", property="og:title")["content"]
-    pl_id = meta["content"][0]+"U"+meta["content"][2:]
+        chl_name = soup.find("meta", property="og:title")["content"]
+        pl_id = meta["content"][0]+"U"+meta["content"][2:]
 
-    return chl_name, pl_id
+        return chl_name, pl_id
+    except [requests.HTTPError, AttributeError] as err:
+        print(err)
+        return "0", "0"
 
 def check_on_new_video(playlist_id, time):
     playlist = service.playlistItems().list(part="contentDetails", playlistId=playlist_id, maxResults=10).execute()
