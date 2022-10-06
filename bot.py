63d46da71feb435bf7cc2bd7d8c4d00f45396b41
ArtemLyah <asuskonotopo@gmail.com>
2022-10-06 09:00:54 +0300

    config

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ import handlers
 if __name__ == "__main__":
     dp.loop.create_task(video_alarm(10))
     print("OK")
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
